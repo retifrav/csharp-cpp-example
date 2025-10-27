a7693237e1e828b03a9b578aeb224152f96d285d
retif <retif@decovar.dev>
2025-10-27 14:59:13 +0100

    C++ library and yet another project restructure

---
+++ cpp/library/include/Thingy/thingy.h
@@ -0,0 +1,12 @@
+#ifndef THINGY_H
+#define THINGY_H
+
+#include <string>
+
+namespace dpndnc
+{
+    std::string doThingy();
+    std::string whoHasTheBestBoobs(std::string jsonString);
+}
+
+#endif // THINGY_H
