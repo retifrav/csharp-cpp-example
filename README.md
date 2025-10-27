a6dbcccfc8e8e2cf82436f903e4bcec960d0fbed
retif <retif@decovar.dev>
2025-10-27 11:23:13 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# .NET/C# and C++ example
+
+An example of using a C++ library inside .NET/C# application.
---
+++ application-csharp/src/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.CommandLine;
+using System.Text;
+
+namespace applctn;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        Option<string> someOption = new("--something", "-s")
+        {
+            Description = "Some thing to pass to the application"
+        };
+
+        RootCommand rootCommand = new("applctn")
+        {
+            Description = "Just some application"
+        };
+        rootCommand.Options.Add(someOption);
+
+        var parseResult = rootCommand.Parse(args);
+        if (parseResult.Errors.Count == 0)
+        {
+            var smOpt = parseResult.GetValue(someOption);
+            if (!string.IsNullOrEmpty(smOpt))
+            {
+                Console.WriteLine($"Something that was passed: {smOpt}");
+            }
+
+            return 0;
+        }
+        else
+        {
+            foreach (var parseError in parseResult.Errors)
+            {
+                Console.Error.WriteLine(parseError.Message);
+            }
+
+            return 1;
+        }
+    }
+}
