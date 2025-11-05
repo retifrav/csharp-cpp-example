e6ac5e64fea11d3802a400203fdf89ffddd6eae0
retif <retif@decovar.dev>
2025-11-05 10:51:01 +0100

    The original C++ library does not have to be hardcoded to SHARED

--- cpp/library/include/Thingy/thingy.h
+++ cpp/library/include/Thingy/thingy.h
@@ -3,14 +3,15 @@
 
 #include <string>
 
-// CMake sets that when BUILD_SHARED_LIBS=1 (or if library is hardcoded to SHARED)
+// CMake sets that when the library is `SHARED` (either hardcoded or when `-DBUILD_SHARED_LIBS=1`)
 #if defined(thingy_EXPORTS)
 //#if defined(thingy_EXPORTS) || defined(thingy_c_api_EXPORTS)
     // export on compiling the DLL (when building)
     #define DLLEXPORT __declspec(dllexport)
-#else
-    // import on using the created DLL (when using in projects)
-    #define DLLEXPORT __declspec(dllimport)
+// that does nothing good when this header is included in other libraries within the project
+//#else
+//    // import on using the created DLL (when using in projects)
+//    #define DLLEXPORT __declspec(dllimport)
 #endif
 
 #ifndef DLLEXPORT
