1c93d9e1448474d1c4f81182898eb493bb96571b
retif <retif@decovar.dev>
2025-11-07 12:45:46 +0100

    Marshalling UTF-8 strings in C++/CLI CLR wrapper too

--- cpp/library-clr/src/thingy-clr.cpp
+++ cpp/library-clr/src/thingy-clr.cpp
@@ -1,3 +1,4 @@
+#include <codecvt>
 #include <msclr/marshal_cppstd.h>
 
 #include <Thingy/thingy.h>
@@ -10,21 +11,34 @@ public:
     static String^ DoThingy()
     {
         std::string thng = dpndnc::doThingy();
-        // using msclr
-        return msclr::interop::marshal_as<String^>(thng);
+
+        // convert from UTF-8 to UTF-16 (System::String)
+        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+        std::wstring thngWstring = converter.from_bytes(thng);
+
+        // return result using msclr
+        return msclr::interop::marshal_as<String^>(thngWstring);
         // or
-        //return gcnew String(thng.c_str());
+        //return gcnew String(thngWstring.c_str());
     }
 
     static String^ WhoHasTheBestBoobs(String^ jsn)
     {
-        std::string bestBoobs = dpndnc::whoHasTheBestBoobs(
-            msclr::interop::marshal_as<std::string>(jsn)
-        );
-        // using msclr
-        return msclr::interop::marshal_as<String^>(bestBoobs);
+        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+
+        // from System::String (UTF-16) to UTF-8
+        std::wstring jsnWstring = msclr::interop::marshal_as<std::wstring>(jsn);
+        std::string jsnString = converter.to_bytes(jsnWstring);
+
+        std::string bestBoobs = dpndnc::whoHasTheBestBoobs(jsnString);
+
+        // from UTF-8 to UTF-16 (System::String)
+        std::wstring bestBoobsWstring = converter.from_bytes(bestBoobs);
+
+        // return result using msclr
+        return msclr::interop::marshal_as<String^>(bestBoobsWstring);
         // or
-        //return gcnew String(bestBoobs.c_str());
+        //return gcnew String(bestBoobsWstring.c_str());
     }
 };
 
