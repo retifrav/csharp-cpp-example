2d9496a2c0107c499f8750fc9044509baad619dc
retif <retif@decovar.dev>
2025-11-07 15:14:31 +0100

    No need to convert UTF-16 to UTF-8 for msclr::interop::marshal_as

--- cpp/library-clr/src/thingy-clr.cpp
+++ cpp/library-clr/src/thingy-clr.cpp
@@ -24,21 +24,39 @@ public:
 
     static String^ WhoHasTheBestBoobs(String^ jsn)
     {
+        // on some () hosts it doesn't seem to be required to do this, as apparently
+        // `msclr::interop::marshal_as<std::string>()` manages `String^` just fine
+        // on its own (converts from UTF-16 to UTF-8 on its own behind the scenes),
+        // but on other hosts that isn't the case for some reason
+        //
+        // for instance, on Windows ARM inside Parallels virtual machine on ARM-based Mac OS
+        // I had to do this
         std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-
+        //
         // from System::String (UTF-16) to UTF-8
         std::wstring jsnWstring = msclr::interop::marshal_as<std::wstring>(jsn);
         std::string jsnString = converter.to_bytes(jsnWstring);
-
+        //
         std::string bestBoobs = dpndnc::whoHasTheBestBoobs(jsnString);
-
+        //
         // from UTF-8 to UTF-16 (System::String)
         std::wstring bestBoobsWstring = converter.from_bytes(bestBoobs);
-
+        //
         // return result using msclr
         return msclr::interop::marshal_as<String^>(bestBoobsWstring);
         // or
         //return gcnew String(bestBoobsWstring.c_str());
+
+        // while on Windows x64 actual physical host it was enough to just do this simple thing
+        /*
+        std::string bestBoobs = dpndnc::whoHasTheBestBoobs(
+            msclr::interop::marshal_as<std::string>(jsn)
+        );
+        // using msclr
+        return msclr::interop::marshal_as<String^>(bestBoobs);
+        // or
+        //return gcnew String(bestBoobs.c_str());
+        */
     }
 };
 
