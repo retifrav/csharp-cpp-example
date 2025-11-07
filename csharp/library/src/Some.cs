9e915651256b615cd49df6554e81d5e2b2daeca1
retif <retif@decovar.dev>
2025-11-07 13:25:16 +0100

    Trying to free the marshalled pointers can crash the application

--- csharp/library/src/Some.cs
+++ csharp/library/src/Some.cs
@@ -24,7 +24,7 @@ public static class Some
     {
         IntPtr thng = do_thingy_c();
         string marshalledString = Marshal.PtrToStringUTF8(thng);
-        Marshal.FreeHGlobal(thng); // need to free the memory
+        //Marshal.FreeHGlobal(thng); // need to free the memory? But it crashes here from time to time
         return marshalledString;
     }
 
@@ -40,7 +40,7 @@ public static class Some
                 jsnPtr
             )
         );
-        Marshal.FreeHGlobal(jsnPtr); // need to free the memory
+        //Marshal.FreeHGlobal(jsnPtr); // need to free the memory? But it crashes here from time to time
         return marshalledString;
     }
 
